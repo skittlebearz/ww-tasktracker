e7c0be1c1ad05ee8cd40bef3c6b99c469f03b0fe
XanderMatheson <mathesonal@gmail.com>
2022-02-17 23:42:13 -0700

    Modified providers and modules so user_project entity could function within project.

--- server/modules/users.module.ts
+++ server/modules/users.module.ts
@@ -11,11 +11,14 @@ import { RefreshTokensController } from 'server/controllers/refresh_tokens.contr
 import { Role } from 'server/entities/role.entity';
 import { RolesService } from 'server/providers/services/roles.service';
 import { UserRole } from 'server/entities/user_role.entity';
+import { Project } from 'server/entities/project.entity';
+import { UserProject } from 'server/entities/user_project.entity';
+import { ProjectsService } from 'server/providers/services/projects.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, RefreshToken, Role, UserRole])],
+  imports: [TypeOrmModule.forFeature([User, RefreshToken, Role, UserRole, Project, UserProject])],
   controllers: [SessionsController, UsersController, RefreshTokensController],
-  providers: [UsersService, RolesService, RefreshTokensService, JwtService],
+  providers: [UsersService, RolesService, ProjectsService, RefreshTokensService, JwtService],
   exports: [TypeOrmModule],
 })
 export class UsersModule {}
