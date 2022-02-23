a759aa59f4e961c4cdab4bf58739f021969b614b
XanderMatheson <mathesonal@gmail.com>
2022-02-22 21:35:11 -0700

    Added ability to update a task's completion status, although it doesn't work in real time.

--- client/components/common/updateButtonVisible.jsx
+++ client/components/common/updateButtonVisible.jsx
@@ -1,5 +1,5 @@
 export const UpdateButtonVisible = ({ task, children, ...other }) => {
-  const visible = !task.completionStatus;
+  var visible = !task.completionStatus;
 
     if (visible) {
       return ( <button className="bg-gray-600 pt-2 pb-2 pr-4 pl-4 rounded-lg font-bold text-white"{...other}>
